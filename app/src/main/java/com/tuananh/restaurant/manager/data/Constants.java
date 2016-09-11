0974b1b0bc6cdab386705088e5ee4f6671dc062e
Vu Tuan Anh <vu.tuan.anh@framgia.com>
2016-09-11 13:37:52 +0700

    create model

---
+++ app/src/main/java/com/tuananh/restaurant/manager/data/Constants.java
@@ -0,0 +1,7 @@
+package com.tuananh.restaurant.manager.data;
+
+/**
+ * Created by framgia on 11/09/2016.
+ */
+public class Constants {
+}
--- app/src/main/java/com/tuananh/restaurant/manager/ui/activity/MainActivity.java
+++ app/src/main/java/com/tuananh/restaurant/manager/ui/activity/MainActivity.java
@@ -1,10 +1,11 @@
-package com.tuananh.showgirl.quanlybanhang;
+package com.tuananh.restaurant.manager.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.tuananh.restaurant.manager.R;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
