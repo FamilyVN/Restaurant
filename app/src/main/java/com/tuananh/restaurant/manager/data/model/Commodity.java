2f6b62cc5f58debcf8c4f104f3f715e9358714d8
Vu Tuan Anh <vu.tuan.anh@framgia.com>
2016-09-11 23:07:11 +0700

    main

---
+++ app/src/main/java/com/tuananh/restaurant/manager/data/model/Commodity.java
@@ -0,0 +1,9 @@
+package com.tuananh.restaurant.manager.data.model;
+
+/**
+ * Created by framgia on 11/09/2016.
+ */
+public class Commodity {
+    private int mId;
+    private int mCost;
+}
--- app/src/main/java/com/tuananh/restaurant/manager/ui/activity/MainActivity.java
+++ app/src/main/java/com/tuananh/restaurant/manager/ui/activity/MainActivity.java
@@ -2,13 +2,40 @@ package com.tuananh.restaurant.manager.ui.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.tuananh.restaurant.manager.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        findViewById(R.id.item_menu_sell).setOnClickListener(this);
+        findViewById(R.id.item_menu_quick_payment).setOnClickListener(this);
+        findViewById(R.id.item_menu_settings).setOnClickListener(this);
+        findViewById(R.id.item_menu_help).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.item_menu_sell:
+                // TODO: 11/09/2016
+                break;
+            case R.id.item_menu_quick_payment:
+                // TODO: 11/09/2016  
+                break;
+            case R.id.item_menu_settings:
+                // TODO: 11/09/2016  
+                break;
+            case R.id.item_menu_help:
+                // TODO: 11/09/2016  
+                break;
+        }
     }
 }
