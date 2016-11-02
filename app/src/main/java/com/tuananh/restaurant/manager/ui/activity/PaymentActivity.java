6cf037e7f06d4f50b9c18a94ca74fdb1bacd6656
Vu Tuan Anh <vu.tuan.anh@framgia.com>
2016-11-02 20:44:46 +0700

    31102016_pay

---
+++ app/src/main/java/com/tuananh/restaurant/manager/ui/activity/PaymentActivity.java
@@ -0,0 +1,13 @@
+package com.tuananh.restaurant.manager.ui.activity;
+
+import android.os.Bundle;
+
+import com.tuananh.restaurant.manager.R;
+
+public class PaymentActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+    }
+}
