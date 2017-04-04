af12353123137f627cf24418cb71342ccc7c786f
Tuan Anh <vtabk2@gmail.com>
2017-04-04 23:21:22 +0700

    ref 20-03-2017

---
+++ app/src/main/java/com/tuananh/restaurant/manager/model/Constant.java
@@ -0,0 +1,7 @@
+package com.tuananh.restaurant.manager.model;
+
+/**
+ * Created by TuanAnh on 3/29/2017.
+ */
+public class Constant {
+}
---
+++ app/src/main/java/com/tuananh/restaurant/manager/model/MenuModel.java
@@ -0,0 +1,22 @@
+package com.tuananh.restaurant.manager.model;
+
+/**
+ * Created by TuanAnh on 4/4/2017.
+ */
+public class MenuModel {
+    private int mType;
+    private int mIdColor;
+    private int mTitle;
+
+    public int getType() {
+        return mType;
+    }
+
+    public int getIdColor() {
+        return mIdColor;
+    }
+
+    public int getTitle() {
+        return mTitle;
+    }
+}
