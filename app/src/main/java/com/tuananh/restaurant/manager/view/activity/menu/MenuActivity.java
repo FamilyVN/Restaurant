adeb1f0892db5ea0d82e7ab50513f65d5403a147
vtabk2 <vtabk22gmail.com>
2017-05-03 13:58:22 +0700

    ref 03_05_2017_create_data_model_item_menu

--- app/src/main/java/com/tuananh/restaurant/manager/view/activity/menu/MenuActivity.java
+++ app/src/main/java/com/tuananh/restaurant/manager/view/activity/menu/MenuActivity.java
@@ -1,13 +1,46 @@
 package com.tuananh.restaurant.manager.view.activity.menu;
 
+import android.view.View;
+import android.widget.AdapterView;
+
 import com.restaurant.tuananh.mvvm.BaseActivityHeaderFooter;
 import com.tuananh.restaurant.manager.R;
 import com.tuananh.restaurant.manager.databinding.ActivityMenuBinding;
+import com.tuananh.restaurant.manager.model.MenuModel;
+import com.tuananh.restaurant.manager.view.adapter.MenuGridViewAdapter;
+
+import java.util.List;
 
 public class MenuActivity
-    extends BaseActivityHeaderFooter<ActivityMenuBinding, MenuActivityViewModel> {
+    extends BaseActivityHeaderFooter<ActivityMenuBinding, MenuActivityViewModel>
+    implements AdapterView.OnItemClickListener {
     @Override
     public int getLayoutId() {
         return R.layout.activity_menu;
+    }
+
+    public void showItemMenu(List<MenuModel> menuModelList) {
+        MenuGridViewAdapter menuGridViewAdapter =
+            new MenuGridViewAdapter(MenuActivity.this, menuModelList);
+        getBinding().setAdapter(menuGridViewAdapter);
+        getBinding().gridViewMenu.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        switch (position) {
+            case MenuGridViewAdapter.POSITION_SELL:
+                // TODO: 03/05/2017
+                break;
+            case MenuGridViewAdapter.POSITION_QUICK_PAYMENT:
+                // TODO: 03/05/2017
+                break;
+            case MenuGridViewAdapter.POSITION_SETTINGS:
+                // TODO: 03/05/2017
+                break;
+            case MenuGridViewAdapter.POSITION_HELP:
+                // TODO: 03/05/2017
+                break;
+        }
     }
 }
--- app/src/main/java/com/tuananh/restaurant/manager/view/activity/menu/MenuActivityViewModel.java
+++ app/src/main/java/com/tuananh/restaurant/manager/view/activity/menu/MenuActivityViewModel.java
@@ -1,10 +1,42 @@
 package com.tuananh.restaurant.manager.view.activity.menu;
 
 import com.restaurant.tuananh.mvvm.BaseViewModel;
+import com.tuananh.restaurant.manager.R;
 import com.tuananh.restaurant.manager.databinding.ActivityMenuBinding;
+import com.tuananh.restaurant.manager.model.MenuModel;
+import com.tuananh.restaurant.manager.view.adapter.MenuGridViewAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by framgia on 03/05/2017.
  */
 public class MenuActivityViewModel extends BaseViewModel<ActivityMenuBinding, MenuActivity> {
+    @Override
+    public void onAttached(boolean isFirst) {
+        super.onAttached(isFirst);
+        createItemMenu();
+    }
+
+    private void createItemMenu() {
+        List<MenuModel> menuModelList = new ArrayList<>();
+        menuModelList.add(new MenuModel(MenuGridViewAdapter.POSITION_SELL,
+            android.R.color.holo_orange_light,
+            R.string.text_menu_sell,
+            R.drawable.img_sell));
+        menuModelList.add(new MenuModel(MenuGridViewAdapter.POSITION_QUICK_PAYMENT,
+            android.R.color.holo_green_light,
+            R.string.text_menu_quick_payment,
+            R.drawable.img_quick_payment));
+        menuModelList.add(new MenuModel(MenuGridViewAdapter.POSITION_SETTINGS,
+            android.R.color.holo_red_light,
+            R.string.text_menu_settings,
+            R.drawable.img_settings));
+        menuModelList.add(new MenuModel(MenuGridViewAdapter.POSITION_HELP,
+            android.R.color.holo_blue_light,
+            R.string.text_menu_help,
+            R.drawable.img_help));
+        getView().showItemMenu(menuModelList);
+    }
 }
