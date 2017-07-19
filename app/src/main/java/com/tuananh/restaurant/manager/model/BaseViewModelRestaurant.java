6a9013291be01fcfa9a8b6459f0d2ee49e618bf5
TuanAnh <vtabk2@gmail.com>
2017-07-19 18:58:53 +0700

    ref setting 19_7

---
+++ app/src/main/java/com/tuananh/restaurant/manager/model/BaseViewModelRestaurant.java
@@ -0,0 +1,13 @@
+package com.tuananh.restaurant.manager.model;
+
+import android.databinding.ViewDataBinding;
+
+import com.restaurant.tuananh.mvvm.BaseViewModel;
+import com.restaurant.tuananh.mvvm.ViewInterface;
+
+/**
+ * Created by FRAMGIA\vu.tuan.anh on 19/07/2017.
+ */
+public class BaseViewModelRestaurant<T extends ViewDataBinding, TV extends ViewInterface>
+    extends BaseViewModel<T, TV> {
+}
---
+++ app/src/main/java/com/tuananh/restaurant/manager/view/activity/settings/ListDrinkFoodActivity.java
@@ -0,0 +1,16 @@
+package com.tuananh.restaurant.manager.view.activity.settings;
+
+import com.tuananh.restaurant.manager.R;
+import com.tuananh.restaurant.manager.databinding.ActivityListDrinkFoodBinding;
+import com.tuananh.restaurant.manager.view.activity.BaseActivityRestaurant;
+
+/**
+ * Created by FRAMGIA\vu.tuan.anh on 19/07/2017.
+ */
+public class ListDrinkFoodActivity
+    extends BaseActivityRestaurant<ActivityListDrinkFoodBinding, ListDrinkFoodActivityViewModel> {
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_list_drink_food;
+    }
+}
